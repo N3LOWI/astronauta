80f2c2fb5bc98e4360ebed88d04789f3a62d3b76
Sachista <97604443+Sachista@users.noreply.github.com>
2022-01-13 12:42:42 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.mycompany.pexamen;
+import java.util.Scanner;
+/**
+ *
+ * @author marce
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Test ob = new Test();
+        
+        ob.intro1();
+        ob.compu1();
+        ob.fin1();
+            
+    }
+}
